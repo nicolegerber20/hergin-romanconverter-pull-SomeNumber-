22c51c018aa876eb2c4aa9ed1afde12685888fac
Nicole Gerber <89939579+nicolegerber20@users.noreply.github.com>
2021-09-09 20:00:08 -0400

    tests code

--- romanconvertor-tests.rb
+++ romanconvertor-tests.rb
@@ -1,62 +1,89 @@
-def fromRoman(romanNumber)
-    # Replace the following line with the actual code!
-    while x<romanNumber.length
-        if romanNumber[x]!="V" && romanNumber[x]!="I"  && romanNumber[x]!="C" &&
-           romanNumber[x]!="L" && romanNumber[x]!="X"  && romanNumber[x]!="D" && romanNumber[x]!="M"
-           raise TypeError
-        elseif romanNumber[x]=="C" && romanNumber[x+1]=="M"
-            total+=900
-            x+=2
-        elseif romanNumber[x]=="X" && romanNumber[x+1]=="M"
-            total+=990
-            x+=2
-        elseif romanNumber[x]=="M"
-            total+=1000
-            x+=1
-        elseif romanNumer[x]=="C" && romanNumber[x+1]=="D"
-            total+=400
-            x+=1
-        elseif romanNumber[x]=="D"
-            total+=500
-            x+=2
-        elseif romanNumber[x]=="X" && romanNumber[x+1]=="C"
-            total+=90
-            x+=2
-        elseif romanNumer[x]=="C"
-            total+=100
-            x+=1
-        elseif romanNumber[x]=="L"
-            total+=50
-            x+=1
-        elseif romanNumber[x]=="X" && romanNumber[x+1]=="L"
-            total+=40
-            x+=2
-        elseif romanNumber[x]=="X"
-            total+=10
-            x+=2
-        elseif romanNumber[x]=="I" && romanNumber[x]=="X"
-            total+=9
-            x+=2
-        elseif romanNumber[x]=="V"
-            total+=4
-            x+=2
-        elseif romanNumber[x]=="I" && romanNumber[x]=="V"
-            total+=4
-            x+=2
-        elseif romanNumber[x]=="I"
-            total+=1
-            x+=1
+# DO NOT CHANGE ANYTHING IN THIS FILE!
+
+require "test/unit"
+require_relative "./romanconvertor"
+
+class TestRomanConvertor < Test::Unit::TestCase
+
+    @@randomValues = {
+        15 => "XV",
+        6 => "VI",
+        78 => "LXXVIII",
+        103 => "CIII"
+    }
+
+    @@specialValues = {
+        1 => "I",
+        5 => "V",
+        10 => "X",
+        50 => "L",
+        100 => "C",
+        500 => "D",
+        1000 => "M"
+    }
+
+    def test_toRoman_repeatingSingleDigit
+        assert_equal("I", toRoman(1))
+        assert_equal("III", toRoman(3))
+    end
+
+    def test_toRoman_randomSuccessfulValues
+        @@randomValues.each{ |key, value| assert_equal(value, toRoman(key)) }
+    end
+
+    def test_fromRoman_randomSuccessfulValues
+        @@randomValues.each{ |key, value| assert_equal(key, fromRoman(value)) }
+    end
+
+    def test_toRoman_specialValues
+        @@specialValues.each{ |key, value| assert_equal(value, toRoman(key)) }
+    end
+
+    def test_fromRoman_specialValues
+        @@specialValues.each{ |key, value| assert_equal(key, fromRoman(value)) }
+    end
+
+    def test_sanity
+        original = "MCDXCVIII"
+        convertedArabic = fromRoman(original)
+        convertedRoman = toRoman(convertedArabic)
+        assert_equal(original, convertedRoman)
+    end
+
+    def test_negativeNumber
+        assert_raise RangeError do
+            toRoman(-1)
         end
     end
-    return total
-end
-    
 
-        
-    raise NotImplementedError
-end
+    def test_number0
+        assert_raise RangeError do
+            toRoman(0)
+        end
+    end
 
-def toRoman(arabicNumber)
-    # Replace the following line with the actual code!
-    raise NotImplementedError
+    def test_number4000
+        assert_raise RangeError do
+            toRoman(4000)
+        end
+    end
+
+    def test_numberMoreThan4000
+        assert_raise RangeError do
+            toRoman(4001)
+        end
+    end
+
+    def test_fromRomanInvalidChars
+        assert_raise TypeError do
+            fromRoman("hwiureh")
+        end
+    end
+
+    def test_fromRomanValidButLowerCase
+        assert_raise TypeError do
+            fromRoman("ii")
+        end
+    end
+   
 end
